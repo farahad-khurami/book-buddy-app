32660d0f2b709fa5ccfff3c74e76b5825732392c
Farahad Khurami <fkhurami98@gmail.com>
2025-03-25 22:41:16 +0000

    change main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.routes import recommend
+from routes import recommend
+import uvicorn
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
 app.include_router(recommend.router, prefix="/recommend")
 
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
