577a4adfeda0efe807152e0389d0dc45dfddd19f
Farahad Khurami <fkhurami98@gmail.com>
2025-03-25 23:02:48 +0000

    allow CORS for vite react frontend

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,6 +5,14 @@ import uvicorn
 
 app = FastAPI(debug=True)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(recommend.router, prefix="/recommend")
 
 if __name__ == "__main__":
