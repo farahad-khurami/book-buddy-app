dd77b74ec7b507713ea42a51fccff427787fa4b2
Farahad Khurami <fkhurami98@gmail.com>
2025-03-28 20:55:31 +0000

    Dockerize backend & add a shell script for running docker

--- backend/src/main.py
+++ backend/src/main.py
@@ -19,4 +19,4 @@ app.include_router(emotion_route.router)
 app.include_router(genre_route.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
