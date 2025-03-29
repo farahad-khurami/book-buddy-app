552642d237f1677e31849889e6ace3c78087d1a5
Farahad Khurami <fkhurami98@gmail.com>
2025-03-29 17:15:17 +0000

    Add 502 response for auth errors with openai

--- backend/src/routes/emotion_route.py
+++ backend/src/routes/emotion_route.py
@@ -1,13 +1,25 @@
 from fastapi import APIRouter, HTTPException
-from models import EmotionRequest, EmotionRecommendationResponse
-from services.emotion_logic import book_emotion_recommendation
+from src.models import EmotionRequest, EmotionRecommendationResponse
+from src.services.emotion_logic import book_emotion_recommendation
+from openai import AuthenticationError
+import logging
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
+
 
 @router.post("/recommend_emotion", response_model=EmotionRecommendationResponse)
 async def recommend_books_by_emotion(user_request: EmotionRequest):
     try:
         books = book_emotion_recommendation(user_request.mood)
         return books
+    except AuthenticationError as e:
+        logger.error("OpenAI authentication failed.")
+        raise HTTPException(
+            status_code=502,
+            detail="There was a problem connecting to the recommendation engine.",
+        )
     except Exception as e:
-        raise HTTPException(status_code=500)
+        logger.exception("Unexpected error occurred.")
+        raise HTTPException(status_code=500, detail="An unexpected error occurred.")
--- backend/src/routes/genre_route.py
+++ backend/src/routes/genre_route.py
@@ -1,14 +1,26 @@
 from fastapi import APIRouter, HTTPException
-from models import GenreRequest, GenreRecommendationResponse
-from services.genre_logic import book_genre_recommendation
+from src.models import GenreRequest, GenreRecommendationResponse
+from src.services.genre_logic import book_genre_recommendation
+from openai import AuthenticationError
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 router = APIRouter()
+
 
 @router.post("/recommend_genre", response_model=GenreRecommendationResponse)
 async def recommend_books_by_genre(user_request: GenreRequest):
     try:
         books = book_genre_recommendation(user_request.genre)
         return books
+    except AuthenticationError as e:
+        logger.error("OpenAI authentication failed.")
+        raise HTTPException(
+            status_code=502,
+            detail="There was a problem connecting to the recommendation engine.",
+        )
     except Exception as e:
-        raise HTTPException(status_code=500)
+        logger.exception("Unexpected error occurred.")
+        raise HTTPException(status_code=500, detail="An unexpected error occurred.")
