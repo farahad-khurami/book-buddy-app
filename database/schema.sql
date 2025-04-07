426471dc921c0c72b8d5b99ea6c597064b89dc9e
Farahad Khurami <fkhurami98@gmail.com>
2025-04-07 20:03:21 +0100

    database setup

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,6 @@
-# PostgreSQL database setup and schema definition
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    name TEXT,
+    picture TEXT
+);
