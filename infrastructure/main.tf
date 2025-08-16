869ee9297f72c9a8dea9b1cdc644011b86281996
Farahad Khurami <fkhurami98@gmail.com>
2025-08-16 21:30:00 +0100

    add force-delete to ecs

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,14 @@
 resource "aws_ecr_repository" "backend_ecr_repo" {
   name = "app-backend-repo"
+  force_delete = true
 }
 
 resource "aws_ecr_repository" "frontend_ecr_repo" {
   name = "app-frontend-repo"
+  force_delete = true
 }
 
 resource "aws_ecs_cluster" "book-buddy-cluster" {
   name = "app-cluster"
+
 }
