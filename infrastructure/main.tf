dcda4f6a61fcba26b1f43712c7ac36ff415c64c0
Farahad Khurami <fkhurami98@gmail.com>
2025-08-16 21:28:52 +0100

    add ecs cluster

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,3 +5,7 @@ resource "aws_ecr_repository" "backend_ecr_repo" {
 resource "aws_ecr_repository" "frontend_ecr_repo" {
   name = "app-frontend-repo"
 }
+
+resource "aws_ecs_cluster" "book-buddy-cluster" {
+  name = "app-cluster"
+}
