0eb43b1420ceea3f232ca909c1a331303f0f89d9
Farahad Khurami <fkhurami98@gmail.com>
2025-08-17 15:44:39 +0100

    add comments [skip ci]

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,13 +1,16 @@
+# Backend ECS Repo
 resource "aws_ecr_repository" "backend_ecr_repo" {
   name = "app-backend-repo"
   force_delete = true
 }
 
+# Frontend ECS Repo
 resource "aws_ecr_repository" "frontend_ecr_repo" {
   name = "app-frontend-repo"
   force_delete = true
 }
 
+# ECS Cluster
 resource "aws_ecs_cluster" "book-buddy-cluster" {
   name = "app-cluster"
 
